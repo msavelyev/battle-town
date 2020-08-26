ace2e52dea37b5d3f05e1c64d9ca9ce7f94ed4c6
Mikhail Savelyev <michael.savelyev@gmail.com>
2020-08-26 16:25:32 +0200

    Configure http port via env variables

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import * as http from 'http'
 import GameServer from './proto/game/GameServer.js';
 import WrtcServer from './proto/wrtc/WrtcServer.js';
+import * as process from 'process';
 
 const app = express();
 const server = http.createServer(app);
@@ -10,6 +11,7 @@ app.use('/', express.static('../web-build/dist'));
 
 GameServer.create(new WrtcServer(app));
 
-server.listen(8080, () => {
-  console.log('listening on 8080');
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
