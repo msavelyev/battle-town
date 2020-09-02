ee604914cdaf803ed22b1c2ed092eeaf03a29705
Mikhail Savelyev <michael.savelyev@gmail.com>
2020-09-02 15:28:45 +0200

    Replace some block textures

--- web/src/game/renderer/Sprites.js
+++ web/src/game/renderer/Sprites.js
@@ -2,9 +2,9 @@
 export default class Sprites {
   constructor(img) {
     createImageBitmap(img, 0, 32, 32, 32).then(img => {this.tank = img});
-    createImageBitmap(img, 16, 0, 16, 16).then(img => {this.brick = img});
-    createImageBitmap(img, 32, 0, 16, 16).then(img => {this.stone = img});
-    createImageBitmap(img, 48, 0, 16, 16).then(img => {this.jungle = img});
+    createImageBitmap(img, 80, 16, 16, 16).then(img => {this.brick = img});
+    createImageBitmap(img, 80, 0, 16, 16).then(img => {this.stone = img});
+    createImageBitmap(img, 96, 0, 16, 16).then(img => {this.jungle = img});
     createImageBitmap(img, 0, 16, 16, 16).then(img => {this.water1 = img});
     createImageBitmap(img, 16, 16, 16, 16).then(img => {this.water2 = img});
     createImageBitmap(img, 32, 16, 8, 8).then(img => {this.bullet = img});
