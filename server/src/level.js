75efbf6befce48e418ef561050bedfdd9ec57655
Mikhail Savelyev <michael.savelyev@gmail.com>
2020-09-08 18:54:42 +0200

    Create first level with spawning positions

--- server/src/level.js
+++ server/src/level.js
@@ -1,10 +1,11 @@
 import Block from '../../lib/src/Block.js';
 import BlockType from '../../lib/src/BlockType.js';
 import Point from '../../lib/src/Point.js';
+import randomInt from '../../lib/src/randomInt.js';
 
 const level = `
 1111111111111111111111111
-1000000000000000000000001
+1000000000000000000000051
 1000000000000000000000001
 1000000000000000000000001
 1000000000000000000000001
@@ -19,19 +20,51 @@ const level = `
 1000000000000000000000001
 1000000000000000000000001
 1000000000000000000000001
-1000000000000000000000001
+1500000000000000000000001
 1111111111111111111111111
 `;
+
+const levels = [
+  `
+    1111111111111111111111111
+    1010000000202020002020151
+    1010222222202020222020101
+    1010200000202000202020101
+    1010202020202220202000101
+    1010202024244422202020101
+    1010202024222444002020101
+    1010002024442422222020101
+    1000222022224424000020001
+    1000200004244222202220001
+    1010202222242444202000101
+    1010202004442224202020101
+    1010202022244424202020101
+    1010002020222020202020101
+    1010202020002020000020101
+    1010202220202022222220101
+    1510202000202020000000101
+    1111111111111111111111111
+  `
+]
 
 const BLOCKS_PER_CELL = 2;
 
 export default function initLevel() {
   const blocks = [];
+
+  const level = levels[randomInt(0, levels.length - 1)];
+
   level.trim().split('\n').forEach((row, y) => {
-    row.split('').forEach((cell, x) => {
+    row.trim().split('').forEach((cell, x) => {
       const blockType = parseInt(cell);
 
       if (blockType === BlockType.EMPTY) {
+        return;
+      }
+
+      if (blockType === BlockType.SPAWN) {
+        const point = new Point(x * BLOCKS_PER_CELL, y * BLOCKS_PER_CELL);
+        blocks.push(new Block(point, blockType));
         return;
       }
 
